664f5cebbda4882a2075aa4b46bb965b399936f2
Aham0803 <ahamgolash9340@gmail.com>
2026-05-23 23:20:36 +0530

    notes

---
+++ 4_Session/4_case_study.py
@@ -0,0 +1,27 @@
+# WAP to identify prime number from a list of numbers
+
+#list =[2,3,5,7,9,11,13,15,17,19,20]
+#def prime(n):
+#    if n < 2:
+#        return "not prime"
+#    for i in range(2,n):
+#        if(n % i == 0):
+#            return "not prime"
+#    return "prime"
+#        
+#for num in list :
+#    print(num ,"=" ,prime(num))
+
+import math
+def is_prime(n):
+    if n <= 1:
+        return False
+    for i in range(2,int(math.sqrt(n))+1):
+        if n % i == 0:
+            return False
+    return True
+
+list = [2,3,5,7,9,11,13,15,17,19,20]
+prime_numbers = [num for num in list if is_prime(num)]
+
+print("prime number is the list" , prime_numbers)
