234790e90d59441c82145e4b04d1d15a3066aee1
Aham0803 <ahamgolash9340@gmail.com>
2026-05-24 14:01:17 +0530

    notes

--- 5_Session.py/4_Problem.py
+++ 5_Session.py/4_Problem.py
@@ -1,28 +1,45 @@
-# to calculate number of uppercase and lowercase character.
+# # to calculate number of uppercase and lowercase character.
 
-Line = " The quick Brown For jumps Over a Lazy Dog"
+# Line = " The quick Brown For jumps Over a Lazy Dog"
 
-n = len(Line)
+# n = len(Line)
 
 
-uppercase = 0
-lowercase = 0
+# uppercase = 0
+# lowercase = 0
 
-upperletter = ""
-lowerletter = ""
-for i in Line:
+# upperletter = ""
+# lowerletter = ""
+# for i in Line:
+#     if i.isupper():
+#         uppercase += 1
+#         upperletter += i
+#     elif i.islower():
+#         lowercase += 1
+#         lowerletter +=i
+
+# print("number of uppercase is " ,uppercase)
+# print("upperletters are : " ,upperletter)
+
+# print("number of lowercase is " ,lowercase)
+# print("lowerletters are : " ,lowerletter)
+
+
+# another method using dictionary to solve this problemm
+# method 2
+
+sentence = " The quick Brown For jumps Over a Lazy Dog"
+counts ={'upper' : 0 , 'lower' : 0}
+
+for i in sentence:
     if i.isupper():
-        uppercase += 1
-        upperletter += i
+        counts['upper'] += 1
     elif i.islower():
-        lowercase += 1
-        lowerletter +=i
+        counts['lower'] += 1
 
-print("number of uppercase is " ,uppercase)
-print("upperletters are : " ,upperletter)
+print("number of uppercase is " ,counts['upper'])
+print("number of uppercase is " ,counts['lower'])
 
-print("number of lowercase is " ,lowercase)
-print("lowerletters are : " ,lowerletter)
 
 
 
