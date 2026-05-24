08f156d4825f61a7ddbf59a4ab65d9d9ea88a1a2
Aham0803 <ahamgolash9340@gmail.com>
2026-05-24 13:19:38 +0530

    notes

---
+++ 5_Session.py/3_reverse_string.py
@@ -0,0 +1,16 @@
+# method 1
+
+def reverse_string(str1):
+    rstr1 = ''
+    index = len(str1)
+    while index > 0:
+        rstr1 += str1[index -1] 
+        index -= 1
+    return rstr1
+print(reverse_string("Gaurav"))
+
+# Method 2
+
+def reverse_string1(itr):
+    return itr[::-1]
+print(reverse_string("Gaurav"))
