44fb9528a3ba738a7364f858e1b95f8b29d9a835
Aham0803 <ahamgolash9340@gmail.com>
2026-05-25 23:08:04 +0530

    notes

--- 6_Session/1_reverse_list.py
+++ 6_Session/1_reverse_list.py
@@ -1,3 +1,14 @@
+#method 1
 li = [1,2,3,4,5]
 li.reverse()
 print(li)
+
+#method 2
+a = [10,20,30,40,50]
+rev = list(reversed(a))
+print(rev)
+
+# method 3
+b = [10,20,30,40,50]
+rever = b[::-1]
+print(rever)
