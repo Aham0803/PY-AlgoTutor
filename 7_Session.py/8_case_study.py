cd26f916398b94e5fb851b565fe8fab0ab1a1c18
Aham0803 <ahamgolash9340@gmail.com>
2026-05-26 23:35:24 +0530

    notes

---
+++ 7_Session.py/8_case_study.py
@@ -0,0 +1,5 @@
+# Write a program to create temperature class that stores a temperature in celsius. add two methods to_farenhit() , to_kelvin(). which returns value of temperature in fahrenhit and kelvin.
+# input : t = Temperature(100)
+#output : celsius : 100
+#         Fahrenhit : 212.0
+#         kelvin : 373.15
