65563342ce03ce9e64183ced87068be839217038
Aham0803 <ahamgolash9340@gmail.com>
2026-05-26 11:57:40 +0530

    notes

---
+++ 7_Session.py/1_Dictionaries_method.py
@@ -0,0 +1,4 @@
+dict = {"name" : "Gaurab" , "age" : 25}
+print(dict.keys()) # returns view obj of all keys
+print(dict.values()) # returns a view obj of all values
+print(dict.items()) # returns key value pairs as a list of tuple
